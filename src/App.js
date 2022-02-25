668b7e9af6467dad8789f27513cbb20548b0230c
Chima Okoro <chimaokoro42@gmail.com>
2022-02-25 16:19:37 +0100

    Made some changed to the files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import { render } from "@testing-library/react";
+import React, { Component }from "react";
+import CardList from "./CardList";
+import { robots } from "./robots";
+import SearchBox from "./SearchBox"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component{ 
+    constructor(){
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ""
+
+        }
+    } 
+
+    onSearchChange(event){
+        console.log(event.target.value)
+
+    }
+
+    render(){
+        return (
+            <div className="tc">
+                <h1>Robot Friends</h1>
+                <SearchBox searchchange = {this.onSearchChange}/>
+                <CardList robots = {this.state.robots}/>
+            </div>
+        )
+        }
+    
 }
 
 export default App;
